36900ce73df855353e51fe36918a40de1a05331a
Marla Brizel <marlabrizel@gmail.com>
2015-09-19 11:41:30 -0600

    wire up login with placeholder dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,6 @@
+class DashboardController < ApplicationController
+  before_action :authorize!
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/auth/strava/callback', to: 'sessions#create'
+  get '/dashboard', to: 'dashboard#show'
 end
