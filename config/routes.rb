10df1d84c533f2208827e697b56b3259c5657826
Marla Brizel <marlabrizel@gmail.com>
2015-09-19 17:30:41 -0600

    user can log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/strava/callback', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   get '/dashboard', to: 'dashboard#show'
 end
