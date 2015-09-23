3a9b67ac9bc60046e4ec239241ec23a2716e50a1
Marla Brizel <marlabrizel@gmail.com>
2015-09-23 09:19:01 -0600

    Dynamically display distance and time, formatted
    Format distance and time outputs in a human-readable manner
    TODO: refactor duplication in models

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,3 +1,17 @@
 class Segment < ActiveRecord::Base
   belongs_to :user
+
+  def hours
+    (elapsed_time / 3600.0).floor
+  end
+
+  def minutes
+    raw = ((elapsed_time % 3600.0) / 60.0).floor
+    raw.to_s.rjust(2, "0")
+  end
+
+  def seconds
+    raw = (elapsed_time % 3600) % 60
+    raw.to_s.rjust(2, "0")
+  end
 end
