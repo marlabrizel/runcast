e0342641bc1a77a3fa391ad0cbd4b79c5b8df005
Marla Brizel <marlabrizel@gmail.com>
2015-09-24 17:35:52 -0600

    WIP user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "retrieving a user's segments" do
+    let(:user) { User.create!(uid: 1, firstname: "Bob") }
+    let(:segment1) { Segment.create!(name: "Vail Pass", elapsed_time: 35, user_id: user.id) }
+    let(:segment2) { Segment.create!(name: "Vail Pass", elapsed_time: 74, user_id: user.id) }
+    let(:segment3) { Segment.create!(name: "Grand Traverse", elapsed_time: 20, user_id: user.id) }
+
+    it "returns a list of unique segment names" do
+      binding.pry
+      expect(user.segment_names).to eq "halp"
+    end
+  end
 end
