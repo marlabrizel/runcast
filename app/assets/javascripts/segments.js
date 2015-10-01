3e85ef4ba3d28fc59c3d1611d53193df4803aeeb
Marla Brizel <marlabrizel@gmail.com>
2015-10-01 11:01:23 -0600

    moved map js into the ajax call
    use javascript to parse javascript

--- app/assets/javascripts/segments.js
+++ app/assets/javascripts/segments.js
@@ -13,6 +13,31 @@ $(document).ready(function() {
       data: { segment_name: data },
       success: function(data) {
         segmentDetails.replaceWith(data);
+        var segmentDataDiv = $("#pr-segment-details");
+        console.log(segmentDataDiv, segmentDataDiv.data());
+        var centerLat = segmentDataDiv.data('startLat');
+        var centerLong = segmentDataDiv.data('startLong');
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: {lat: centerLat, lng: centerLong},
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        });
+
+        var path = segmentDataDiv.data('polyline');
+        var decodedSets = google.maps.geometry.encoding.decodePath(path)
+
+        var activityCoordinates = decodedSets;
+
+        var activity = new google.maps.Polyline({
+          path: activityCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        activity.setMap(map);
       }, error: function(xhr) {
         console.log(xhr);
       }
